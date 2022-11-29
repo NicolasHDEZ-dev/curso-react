f4ced06d175b291842f670cdddf367b840485577
Nics <nicsmusiccontact@gmail.com>
2022-11-29 19:11:57 -0300

    primer-entrega

--- src/components/ItemListContainer/ItemListContainer.js
+++ src/components/ItemListContainer/ItemListContainer.js
@@ -1,9 +1,10 @@
 import React from 'react'
+import './titulo.css'
 
 const ItemListContainer = ({greeting}) => {
   return (
     <div>
-      <h2 className="titulo1">{greeting}</h2>
+      <h2 className="bienvenida">{greeting}</h2>
     </div>
   )
 }
