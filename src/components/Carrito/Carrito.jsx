015d8cb7a64e91c8584f02c615bc081db78b233a
Nics <nicsmusiccontact@gmail.com>
2023-02-20 14:34:12 -0300

    ai

--- src/components/Carrito/Carrito.jsx
+++ src/components/Carrito/Carrito.jsx
@@ -7,87 +7,83 @@ import './Carrito.css';
 import Formulario from '../Form/Formulario';
 
 
-const Carrito = ({formData}) => {
+const Carrito = () => {
 
   const [orderId , setOrderId] = useState()
 
-const {carrito, totalPrice} = useCartContext();
-
-const orden = {
-
-comprador: {
-  nombre: "",
-  email: "pedro@gmail.com",
-  telefono: "1132435673",
-  direccion: "Pepito 423"
-},
-items: carrito.map(product => ({id: product.id, nombre: product.nombre, precio: product.precio,})),
-total: totalPrice(),
-
-}
-
-const handleClick = () => {
-const db = getFirestore();
-const ordenesCollection = collection(db,'ordenes');
-addDoc(ordenesCollection, orden)
-.then((({id})=>{
-  setOrderId(id)
-  .clear()
-  }))}
-
-if(orderId){
-  return(
-  <>
-  <div className='container justify-content-center'>
-<div className='tarjeta bg-white'>
-<h1> Gracias por su compra </h1>
-<p>Su orden de compra es {orderId}</p>
-<Link className='btn btn-success' to='/'>Volver a Comprar</Link>
-</div>
-</div>
-</>
-  )
-}
-
-if(carrito.length===0 && !orderId){
+  const {carrito, totalPrice, clearCart} = useCartContext();
+  
+  
+  
+  const handleClick = (comprador) => {
+    let orden = {
+  
+      comprador,
+      items: carrito,
+      total: totalPrice(),
+      
+      }
+  const db = getFirestore();
+  const ordenesCollection = collection(db,'ordenes');
+  addDoc(ordenesCollection, orden)
+  .then((({id})=>{
+    setOrderId(id)
+    clearCart()
+    }))}
+  
+  if(orderId){
     return(
+    <>
+    <div className='container justify-content-center'>
+  <div className='tarjeta bg-white'>
+  <h1> Gracias por su compra </h1>
+  <p>Su orden de compra es {orderId}</p>
+  <Link className='btn btn-success' to='/'>Volver a Comprar</Link>
+  </div>
+  </div>
+  </>
+    )
+  }
+  
+  if(carrito.length===0 && !orderId){
+      return(
+        <>
+        <div className='container-fluid m-3 p-3 justify-content-center align-items-center'>
+        <div className='container-fluid justify-content-center '>
+        <div className='tarjeta text-center'>
+        <p className='fs-2'>El Carrito esta vacio!</p>
+        <Link to='/catalogo' className='btn btn-danger'>Porque no compras algo...?</Link>
+        </div>
+        </div>
+        </div>
+        </>
+      );
+    }
+  
+    
+    return (
       <>
-      <div className='container-fluid m-3 p-3 justify-content-center align-items-center'>
-      <div className='container-fluid justify-content-center '>
-      <div className='tarjeta text-center'>
-      <p className='fs-2'>El Carrito esta vacio!</p>
-      <Link to='/catalogo' className='btn btn-danger'>Porque no compras algo...?</Link>
+      <h1 className='text-center'>Este es tu carrito</h1>
+      <div className='row row-cols-1 row-cols-md-3 g-4 mt-3'>
+      {
+        carrito.map(product => <CarritoItem key={product.id} product={product}/>)
+      }
       </div>
+      <section className='container__form text-center'>
+                      <h4 className='form__title'>Formulario de compra</h4>
+                      <div className='cart__total'>
+                          <p className='cart__total--p'>
+                              Total de compra  <span>$ARS {totalPrice()}</span>
+                          </p>
+                      </div>
+                      <Formulario handleClick = {handleClick}/>   
+                  </section>
+                  <div className=' text-center'>
+     
       </div>
-      </div>
+      
       </>
     );
-  }
-
   
-  return (
-    <>
-    <h1 className='text-center'>Este es tu carrito</h1>
-    <div className='row row-cols-1 row-cols-md-3 g-4 mt-3'>
-    {
-      carrito.map(product => <CarritoItem key={product.id} product={product}/>)
     }
-    </div>
-    <section className='container__form text-center'>
-                    <h4 className='form__title'>Formulario de compra</h4>
-                    <div className='cart__total'>
-                        <p className='cart__total--p'>
-                            Total de compra  <span>$ARS {totalPrice()}</span>
-                        </p>
-                    </div>
-                    <Formulario/>   
-                </section>
-                <div className=' text-center'>
-    <button className='btn btn-success' onClick={handleClick}>Emitir Compra</button>
-    </div>
-    
-    </>
-  );
-
-  }
 export default Carrito
--- src/components/Form/Formulario.jsx
+++ src/components/Form/Formulario.jsx
@@ -7,43 +7,63 @@ import './Formulario.css'
 
 
 
+const Formulario = ({handleClick}) => {
 
-const Formulario = () => {
 
-    
+    const {validarFormulario } = useCartContext()
+
+
+    const [comprador, setComprador]= useState({})
+    const datosComprador = (e)=> {
+        setComprador({
+            ...comprador,
+           [e.target.name]:e.target.value
+        })
+    }
+    const sendOrder = (e) =>{
+        e.preventDefault();
+        if(validarFormulario(datosComprador)){
+            console.log("hola")
+        }
+        handleClick(comprador)
+    }
     return (
     <>
-        <form className='form__cart'>
+        <form className='form__cart' onSubmit={sendOrder}>
             <input
                 className='form__text'
                 type="text"
                 name="nombre"
                 placeholder="Nombre"
-                
+                onChange={datosComprador}
             />
             <input
                 className='form__text'
                 type="text"
                 name="apellido"
                 placeholder="Apellido"
+                onChange={datosComprador}
             />
             <input
                 className='form__text '
                 type="email"
                 name="email"
-                placeholder="Email"               
+                placeholder="Email" 
+                onChange={datosComprador}              
             />
             <input
                 className='form__text'
                 type="text"
                 name="telefono"
-                placeholder="Telefono"            
+                placeholder="Telefono" 
+                onChange={datosComprador}           
             />
             <textarea
                 className='form__text'
                 type="text"
                 name="mensaje"
-                placeholder="Escriba su mensaje"               
+                placeholder="Escriba su mensaje" 
+                            
             />
             <button className='btn__cart mt-2' type="submit">Enviar</button>
         </form>
