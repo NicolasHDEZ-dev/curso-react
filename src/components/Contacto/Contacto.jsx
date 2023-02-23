9107608715e5fabefe6591689a78ea6e065373e4
Nics <nicsmusiccontact@gmail.com>
2023-02-23 14:12:37 -0300

    update last

--- src/components/Contacto/Contacto.jsx
+++ src/components/Contacto/Contacto.jsx
@@ -8,10 +8,10 @@ export const Contacto = () => {
     <div className='d-flex mt-2 container-fluid justify-content-center'>
     <div className='tarjetas'>
       <div className='face front'>
-      <img src="https://cdn-icons-png.flaticon.com/512/124/124010.png" alt="" />
+      <img src="https://cdn-icons-png.flaticon.com/512/124/124010.png" alt=""/>
       <h2>Facebook</h2>
       </div>
-      <div className='face back'>
+      <div className='face back letras'>
         <h2>Facebook</h2>
         <p>Luxury Cars es nuestro usuario de facebook!</p>
       </div>
@@ -22,7 +22,7 @@ export const Contacto = () => {
       <img src="https://i0.wp.com/eltallerdehector.com/wp-content/uploads/2022/06/cd939-logo-instagram-png.png?fit=512%2C512&ssl=1" alt="" />
       <h2>Instagram</h2>
       </div>
-      <div className='face back'>
+      <div className='face back letras'>
         <h2>Instagram</h2>
         <p>Encontranos en instagram como luxury.cars1!</p>
       </div>
@@ -30,10 +30,10 @@ export const Contacto = () => {
 
     <div className='tarjetas'>
       <div className='face front'>
-      <img src="https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c53e.png" alt="" />
+      <img src="https://assets.stickpng.com/thumbs/580b57fcd9996e24bc43c53e.png" alt=""/>
       
       </div>
-      <div className='face back'>
+      <div className='face back letras'>
         <h2>Twitter</h2>
         <p>Nuestro usuario de twitter es @luxurycars1</p>
       </div>
